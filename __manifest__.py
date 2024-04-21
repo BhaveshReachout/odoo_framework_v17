b9a7d11a3bbe59d0d95105d6d4c501bdb8d7828c
BhaveshReachout <bhavesh.reachout@gmail.com>
2024-04-21 09:10:53 +0530

    initilise init & manifest

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'odoo framework',
+    'version': '1.0',
+    'website': '',
+    'category': 'Sales',
+    'summary': '',
+    'description': """ """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'assets': {
+        'web.assets_backend': [
+        ],
+        'web.assets_frontend': [
+        ],
+        'web.report_assets_common': [
+        ],
+        'web.report_assets_pdf': [
+        ],
+    },
+    'license': 'LGPL-3',
+}
